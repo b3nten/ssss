46d91ab743a02fffbf835543aa6704c9d679c53a
benton <ben@benten.ca>
2025-12-03 14:32:19 -0800

    Update schema globals to use index operator as field index setter

--- parser/schema_globals.lua
+++ parser/schema_globals.lua
@@ -1,26 +1,32 @@
 local function uuid()
-    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-    return string.gsub(template, '[xy]', function (c)
-        local v = (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
-        return string.format('%x', v)
-    end)
+	local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+	return string.gsub(template, '[xy]', function(c)
+		local v = (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
+		return string.format('%x', v)
+	end)
 end
 
--- Helper to make a type callable for metadata
 local function make_type(base_type)
-    return setmetatable(base_type, {
-        __call = function(self, metadata)
-            local result = {}
-            for k, v in pairs(self) do
-                result[k] = v
-            end
-            result.metadata = metadata
-            return result
-        end
-    })
+	local result = {
+		metadata = {},
+	}
+	for k, v in pairs(base_type) do
+		result[k] = v
+	end
+	return setmetatable(result, {
+		__call = function(self, metadata)
+			for k, v in pairs(metadata) do
+				self.metadata[k] = v
+			end
+			return result
+		end,
+		__index = function(self, key)
+			self.metadata.id = key
+			return self
+		end,
+	})
 end
 
--- Primitives
 bool = make_type({ type = "primitive", name = "bool" })
 int8 = make_type({ type = "primitive", name = "int8" })
 uint8 = make_type({ type = "primitive", name = "uint8" })
@@ -30,30 +36,35 @@ int32 = make_type({ type = "primitive", name = "int32" })
 uint32 = make_type({ type = "primitive", name = "uint32" })
 int64 = make_type({ type = "primitive", name = "int64" })
 uint64 = make_type({ type = "primitive", name = "uint64" })
+float32 = make_type({ type = "primitive", name = "float32" })
+float64 = make_type({ type = "primitive", name = "float64" })
 str = make_type({ type = "primitive", name = "string" })
 
-function struct(fields)
-    local s = {
-        type = "struct",
-        fields = fields,
-        uuid = uuid(),
-    }
-    return setmetatable(s, {
-        __call = function(self, metadata)
-            local result = {}
-            for k, v in pairs(self) do
-                result[k] = v
-            end
-            result.metadata = metadata
-            return result
-        end
-    })
-end
-
 function list(type)
-    return make_type({type = "list", of = type})
+	return make_type({ type = "list", of = type })
 end
 
 function map(keyType, valueType)
-    return make_type({type = "map", key = keyType, value = valueType})
+	return make_type({ type = "map", from = keyType, to = valueType })
+end
+
+function struct(fields)
+	local s = {
+		type = "struct",
+		fields = fields,
+		uuid = uuid(),
+		metadata = {},
+	}
+	return setmetatable(s, {
+		__call = function(self, metadata)
+			for k, v in pairs(metadata) do
+				self.metadata[k] = v
+			end
+			return self
+		end,
+		__index = function(self, key)
+			self.metadata.id = key
+			return self
+		end,
+	})
 end
